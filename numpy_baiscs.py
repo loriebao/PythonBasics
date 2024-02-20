d2a27f39e796e2253b2302754881a5a75bc29032
Lorie Bao <loriebao@yahoo.com>
2024-02-19 22:26:57 -0600

    Commit my change

--- numpy_baiscs.py
+++ numpy_baiscs.py
@@ -1,14 +1,14 @@
 #define numpy array
 import numpy as np
 
-np1 = np.array([1, 2, 3])
-np2 = np.array([4, 5, 6])
+np1 = np.array([1, 2, 3], dtype=np.int32)
+np2 = np.array([4, 5, 6], dtype=np.int32)
 
-np3 = np.array([[11, 1, 2], [3, 4, 9], [18, 6, 5]])
-np4 = np.array([[3, 6, 1], [6, 80, 4], [13, 8, 7]])
+np3 = np.array([[11, 1, 2], [3, 4, 9], [18, 6, 5]], dtype=np.int32)
+np4 = np.array([[3, 6, 1], [6, 80, 4], [13, 8, 7]], dtype=np.int32)
 
-A = np.random.randint(1,100,(3,3))
-b = np.random.randint(1, 10,(3))
+A = np.random.randint(1,100,(3,3), dtype=np.int32)
+b = np.random.randint(1, 10,(3), dtype=np.int8)
 
 print( f'add {np.add(np1, np2)}' ) 
 print( f'subtract {np.subtract(np1, np2)}' ) 
@@ -24,15 +24,14 @@ print( f'det {A} {np.linalg.det(A)}' )
 print( f'inv {A} {np.linalg.inv(A)}' ) 
 print( f'solve {A}.x = {b} {np.linalg.solve(A, b)}' ) 
 
-M1 = np.random.randint(1,100,(3,4))
-M2 = np.random.randint(1, 10,(4,2))
-print( f'{M1}.{M2} {np.dot(M1, M2)}' ) 
+M1 = np.random.randint(1,100,(3,4), dtype=np.int32)
+M2 = np.random.randint(1, 10,(4,2), dtype=np.int8)
+M3 = np.dot(M1, M2)
+print( f'{M1}.{M2} {np.dot(M1, M2)} {M1.dtype} {M2.dtype} {M3.dtype}' ) 
 
-# print( f'add {np.add(np1, np2)}' ) 
-# print( f'add {np.add(np1, np2)}' ) 
-# print( f'add {np.add(np1, np2)}' ) 
-  
-
-
+one_matrix = np.ones((6,4))
+print( f'{one_matrix} {np.ndim(one_matrix)} {np.shape(one_matrix)}' )   
    
+c = np.reshape(one_matrix, (2, 3, 4)) # 3d array
+print(c)
 
