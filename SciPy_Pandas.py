a449e86eb68d311742ebb1fb0fb5aec40aac173d
Lorie Bao <loriebao@yahoo.com>
2024-02-28 15:16:39 -0600

    add missing plt.show()

--- SciPy_Pandas.py
+++ SciPy_Pandas.py
@@ -138,3 +138,17 @@ ax.set_xticks(np.arange(-1, 1.1, 0.5))
 ax.set_yticks(np.arange(-1, 1.1, 0.5))
 ax.set_zlabel('Z')
 plt.show()
+
+data1, data2, data3, data4 = np.random.randn(4, 100)  # make 4 random data sets
+def my_plotter(ax, data1, data2, param_dict):
+    """
+    A helper function to make a graph.
+    """
+    out = ax.plot(data1, data2, **param_dict)
+    return out
+
+#which you would then use twice to populate two subplots:
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(5, 2.7))
+my_plotter(ax1, data1, data2, {'marker': 'x'})
+my_plotter(ax2, data3, data4, {'marker': 'o'})
+plt.show()
