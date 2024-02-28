087592503d2daf9b3cced82f4804e889c1e887de
loriebao <loriebao@yahoo.com>
2024-02-28 11:32:23 -0600

    added 3D plot example

--- SciPy_Pandas.py
+++ SciPy_Pandas.py
@@ -98,7 +98,11 @@ angle: The angle between the normal to the drum head and the direction of propag
 J_n(k*r) = C * cos(n*θ) * jn(n, k*r)
 """
 
-def bessel_eq(n: int, k: float, distance: float, angle: float, t: float) -> float:
+def drum_height_bessel_eq(n: int, 
+                          k: float, 
+                          distance: float, 
+                          angle: float, 
+                          t: float) -> float:
     """
     Calculates the Bessel function for a given set of parameters.
 
@@ -118,4 +122,19 @@ def bessel_eq(n: int, k: float, distance: float, angle: float, t: float) -> floa
     kth_zero = special.jn_zeros(n, k)[-1]
     return np.cos(t) * np.cos(n*angle) * special.jn(n, distance*kth_zero)
 
+theta = np.r_[0:2*np.pi:50j]
+radius = np.r_[0:1:50j]
+x = np.array([r * np.cos(theta) for r in radius])
+y = np.array([r * np.sin(theta) for r in radius])
+z = np.array([drum_height_bessel_eq(1, 1, r, theta, 0.5) for r in radius])
 
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_axes(rect=(0, 0.05, 0.95, 0.95), projection='3d')
+ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='RdBu_r', vmin=-0.5, vmax=0.5)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_xticks(np.arange(-1, 1.1, 0.5))
+ax.set_yticks(np.arange(-1, 1.1, 0.5))
+ax.set_zlabel('Z')
+plt.show()
